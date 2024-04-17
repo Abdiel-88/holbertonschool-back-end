7dd0ae6c8f4b0c11ee8fc6a95783e2acb183ed71
Abdiel-88 <abdiel.f@outlook.com>
2024-04-16 20:13:48 -0400

    pain....

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -12,7 +12,7 @@ import sys
 
 
 if __name__ == "__main__":
-    # Get the employee ID from the command-line argument
+    # Get employee ID from the command-line argument
     user_id = sys.argv[1]
 
     # Base URL for the JSONPlaceholder API
@@ -22,7 +22,7 @@ if __name__ == "__main__":
     user = requests.get(url + "users/{}".format(user_id)).json()
     username = user.get("username")
 
-    # Fetch the to-do list for the employee using the provided employee ID
+    # Fetch to-do list for the employee using the provided employee ID
     params = {"userId": user_id}
     todos = requests.get(url + "todos", params).json()
 
@@ -38,6 +38,6 @@ if __name__ == "__main__":
         ]
     }
 
-    # Write the data to a JSON file with the employee ID as the filename
+    # Write data to a JSON file with the employee ID as the filename
     with open("{}.json".format(user_id), "w") as jsonfile:
         json.dump(data_to_export, jsonfile, indent=4)
