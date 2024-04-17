85bfdfa805c69bbbf015805ddc76b1c58ba79366
Abdiel-88 <abdiel.f@outlook.com>
2024-04-16 20:07:17 -0400

    task 0 executable?

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -15,11 +15,11 @@ if __name__ == "__main__":
     # Base URL for the JSONPlaceholder API
     url = "https://jsonplaceholder.typicode.com/"
 
-    # Get the employee information using the provided employee ID
+    # Get employee information using the provided employee ID
     employee_id = sys.argv[1]
     user = requests.get(url + "users/{}".format(employee_id)).json()
 
-    # Get the to-do list for the employee using the provided employee ID
+    # Get to-do list for the employee using the provided employee ID
     params = {"userId": employee_id}
     todos = requests.get(url + "todos", params).json()
 
